86df6c70c0d40718f4eb948ab5d79b8e3b67804d
MD TAWHID ISLAM <lorddanju@gmail.com>
2026-04-01 12:35:36 +0600

    Refactor plugin handlers to include new handlers

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -5,23 +5,25 @@ from .info import setup_info_handler
 from .thumb import setup_thumb_handler
 from .login import setup_login_handler
 from .pbatch import setup_pbatch_handler
-from .ytdl import setup_ytdl_handler        # ← নতুন
-from .refresh import setup_refresh_handler  # ← নতুন
+from .ytdl import setup_ytdl_handler
+from .refresh import setup_refresh_handler
 from .settings import setup_settings_handler
 from .autolink import setup_autolink_handler
 from .referral import setup_referral_handler
 from .gdl import setup_gdl_handler
+from .directdl import setup_directdl_handler   # ← file hosting sites downloader
 
 
 def setup_plugins_handlers(app):
     setup_plan_handler(app)
     setup_gdl_handler(app)
+    setup_directdl_handler(app)                 # ← register here
     setup_info_handler(app)
     setup_thumb_handler(app)
     setup_login_handler(app)
     setup_pbatch_handler(app)
-    setup_ytdl_handler(app)                 # ← নতুন
-    setup_refresh_handler(app)              # ← নতুন
+    setup_ytdl_handler(app)
+    setup_refresh_handler(app)
     setup_settings_handler(app)
     setup_autolink_handler(app)
-    setup_referral_handler(app) 
+    setup_referral_handler(app)
