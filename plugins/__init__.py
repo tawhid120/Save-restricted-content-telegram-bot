95fe26eabbaac4e137dbe3c1b7f02eae0871bdac
MD TAWHID ISLAM <lorddanju@gmail.com>
2026-04-04 18:04:16 +0600

    Add YouTube upload handler to plugin setup

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -6,12 +6,13 @@ from .thumb import setup_thumb_handler
 from .login import setup_login_handler
 from .pbatch import setup_pbatch_handler
 from .ytdl import setup_ytdl_handler
+from .ytupload import setup_ytupload_handler     # ← YouTube Upload handler
 from .refresh import setup_refresh_handler
 from .settings import setup_settings_handler
 from .autolink import setup_autolink_handler
 from .referral import setup_referral_handler
 from .gdl import setup_gdl_handler
-from .directdl import setup_directdl_handler   # ← file hosting sites downloader
+from .directdl import setup_directdl_handler    # ← file hosting sites downloader
 
 
 def setup_plugins_handlers(app):
@@ -23,6 +24,7 @@ def setup_plugins_handlers(app):
     setup_login_handler(app)
     setup_pbatch_handler(app)
     setup_ytdl_handler(app)
+    setup_ytupload_handler(app)                 # ← YouTube Upload register
     setup_refresh_handler(app)
     setup_settings_handler(app)
     setup_autolink_handler(app)
