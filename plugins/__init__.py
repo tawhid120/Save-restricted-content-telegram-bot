2fc31eb156f0757cef694ad6b0529a833ceef09f
MD TAWHID ISLAM <lorddanju@gmail.com>
2026-04-06 22:44:54 +0600

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,5 +1,6 @@
 # Copyright @juktijol
 # Channel t.me/juktijol
+from .auto_router import setup_auto_router   # ← নতুন
 from .plan import setup_plan_handler
 from .info import setup_info_handler
 from .thumb import setup_thumb_handler
@@ -13,11 +14,12 @@ from .autolink import setup_autolink_handler
 from .referral import setup_referral_handler
 from .gdl import setup_gdl_handler
 from .directdl import setup_directdl_handler
-from .auto_router import setup_auto_router   # ← নতুন
+
 
 
 def setup_plugins_handlers(app):
     setup_plan_handler(app)
+     setup_auto_router(app)        # group=3 — বাকি সব লিংক route কর
     setup_gdl_handler(app)
     setup_directdl_handler(app)
     setup_info_handler(app)
@@ -30,4 +32,4 @@ def setup_plugins_handlers(app):
     setup_settings_handler(app)
     setup_autolink_handler(app)   # group=1 — Telegram লিংক handle করে
     setup_referral_handler(app)
-    setup_auto_router(app)        # group=3 — বাকি সব লিংক route করে
+ে
